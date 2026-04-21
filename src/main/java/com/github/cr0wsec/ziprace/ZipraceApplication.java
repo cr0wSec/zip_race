f9447dbb54890ba7b21c93a2efa4b5a15eff2388
John Doe <204260440+cr0wSec@users.noreply.github.com>
2026-04-21 16:52:42 +0200

    add spring boot app entry point

---
+++ src/main/java/com/github/cr0wsec/ziprace/ZipraceApplication.java
@@ -0,0 +1,13 @@
+package com.github.cr0wsec.ziprace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipraceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ZipraceApplication.class, args);
+	}
+
+}
