bdf8a868d3684a119b9ba8761eaef6dda89337fc
John Doe <204260440+cr0wSec@users.noreply.github.com>
2026-04-21 17:08:12 +0200

    add WriteQueueConfig class

--- src/main/java/com/github/cr0wsec/ziprace/concurrent/WriteQueueConfig.java
+++ src/main/java/com/github/cr0wsec/ziprace/concurrent/WriteQueueConfig.java
@@ -1,4 +1,18 @@
 package com.github.cr0wsec.ziprace.concurrent;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+@Configuration
 public class WriteQueueConfig {
+
+    @Bean
+    public BlockingQueue<WriteTask> writeQueue(
+            @Value("${ziprace.queue.capacity}") int capacity) {
+        return new ArrayBlockingQueue<>(capacity);
+    }
 }
